3d55ce566a880aa9e9129bf103d996ef20d7f62b
AronLenvig <AronLenvigg@gmail.com>
2023-09-09 01:47:46 +0100

    movement slighly better?

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,25 @@ function Player({id}: {id: string}) {
         startX: number;
         startY: number;
     }
+
     const elem = React.useRef<HTMLSpanElement>(null);
     const [x, set_x] = React.useState(0);
     const [y, set_y] = React.useState(0);
     const [projectiles, setProjectiles] = React.useState<ProjectileData[]>([]);  // <-- Explicitly type the state
+    const [keysPressed, setKeysPressed] = React.useState({ KeyW: false, KeyS: false, KeyA: false, KeyD: false });
+    const [velocity, setVelocity] = React.useState({ x: 0, y: 0 });
+
+    const MOVE_AMOUNT = 0.5;
+    const FRICTION = 0.9;
+    const MaxSpeed = 10;
 
 
+    const keyToDirectionMap: Record<string, { x: number, y: number }> = {
+        KeyW: { x: 0, y: -MOVE_AMOUNT },
+        KeyS: { x: 0, y: MOVE_AMOUNT },
+        KeyA: { x: -MOVE_AMOUNT, y: 0 },
+        KeyD: { x: MOVE_AMOUNT, y: 0 },
+    };
 
     // React.useEffect(() => {
     //     var elem_input = Input(elem);
@@ -41,20 +54,49 @@ function Player({id}: {id: string}) {
     //         }
     //     });
     // }, [y]);
+    
+    React.useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (keyToDirectionMap[e.code]) {
+                setKeysPressed(prev => ({ ...prev, [e.code]: true }));
+            }
+        };
+    
+        const handleKeyUp = (e: KeyboardEvent) => {
+            if (keyToDirectionMap[e.code]) {
+                setKeysPressed(prev => ({ ...prev, [e.code]: false }));
+            }
+        };
+    
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+    
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keyup', handleKeyUp);
+        };
+    }, []);
+
+    React.useEffect(() => {
+        const move = () => {
+            let deltaX = 0
+            let deltaY = 0;
+    
+            if (keysPressed.KeyW) deltaY -= MOVE_AMOUNT;
+            if (keysPressed.KeyS) deltaY += MOVE_AMOUNT;
+            if (keysPressed.KeyA) deltaX -= MOVE_AMOUNT;
+            if (keysPressed.KeyD) deltaX += MOVE_AMOUNT;
+
+            if (deltaX !== 0) set_x(prev => prev + deltaX);
+            if (deltaY !== 0) set_y(prev => prev + deltaY);
+    
+            requestAnimationFrame(move);
+        };
+    
+        move();
+    }, [keysPressed]);
 
     const handleKeydown = React.useCallback((event: React.KeyboardEvent<HTMLSpanElement>, x: number, y: number) => {
-        if (event.code === "KeyS") {
-            set_y(y + 5);
-        }
-        if (event.code === "KeyW") {
-            set_y(y - 5);
-        }
-        if (event.code === "KeyD") {
-            set_x(x + 5);
-        }
-        if (event.code === "KeyA") {
-            set_x(x - 5);
-        }
         if (event.code === "Space") {
             setProjectiles(prev => [...prev, { startX: x+100, startY: y+100 }]);
         }
