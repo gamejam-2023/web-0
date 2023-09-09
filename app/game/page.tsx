ad1e1bcdb4f29e5293b5f0d294875524c4338694
AronLenvig <AronLenvigg@gmail.com>
2023-09-09 17:52:43 +0100

    fix shootinh

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -272,7 +272,11 @@ function Background() {
     return (
         <>
             <div className="relative w-screen h-screen overflow-hidden"> {/* Container for the waves */}
-                <Image className="absolute w-screen h-screen" src={"/img/BgCore2.png"} alt={""} layout='fill'/>
+                <Image className="absolute w-screen h-screen" src={"/img/BgOnlt.png"} alt={""} layout='fill'/>
+                <Image className="absolute w-screen h-screen" src={"/img/WaterMovingMiddle1.png"} alt={""} layout='fill'/>
+                <Image className="absolute w-screen h-screen WaterMovingMiddle" src={"/img/WaterMovingMiddle2.png"} alt={""} layout='fill'/>
+
+
                 <Image className="absolute w-screen h-screen wavesAnimation" src={"/img/waves2.png"} alt={"Waves"} layout='fill'/>
                 <Image className="absolute w-screen h-screen wavesAnimation" style={{top: '-100%'}} src={"/img/waves2.png"} alt={"Waves"} layout='fill'/>
             </div>
--- app/game/shooting.tsx
+++ app/game/shooting.tsx
@@ -53,7 +53,8 @@ export default function Projectile({startX, startY, IsLeft}: {startX: number, st
         // console.log("prevX: " + prevX);
         const Player_other_x = IsLeft === true ? globalState[2] : globalState[0];
         const Player_other_y = IsLeft === true ? globalState[3] : globalState[1];
-        if (prevX > Player_other_x[0] - 7 && prevX < Player_other_x[0] + 7 && y > Player_other_y[0] - 10 && y < Player_other_y[0] + 10) {
+        console.log("Player_other_y: " + Player_other_y[0]);
+        if (prevX > Player_other_x[0] && prevX < Player_other_x[0] + 6 && y > Player_other_y[0] && y < Player_other_y[0] + 20) {
             console.log("hit");
             Health_other[1](Health_other[0] - 10)
             return -100;
