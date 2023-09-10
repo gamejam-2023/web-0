2d14dc9460760d38d54f8188ed79d9069dbada8f
AronLenvig <AronLenvigg@gmail.com>
2023-09-10 15:10:40 +0100

    allah

--- app/game/background.tsx
+++ app/game/background.tsx
@@ -8,8 +8,10 @@ export function Background() {
                 <Image className="absolute w-screen h-screen" src={"/img/BgOnlt.png"} alt={""} layout='fill'/>
                 <Image className="absolute w-screen h-screen wavesAnimation" src={"/img/waves2.png"} alt={"Waves"} layout='fill'/>
                 <Image className="absolute w-screen h-screen wavesAnimation" style={{top: '-100%'}} src={"/img/waves2.png"} alt={"Waves"} layout='fill'/>
-                <Image className="absolute w-screen h-screen" src={"/img/WaterMovingMiddle1.png"} alt={""} layout='fill'/>
-                <Image className="absolute w-screen h-screen WaterMovingMiddle" src={"/img/WaterMovingMiddle2.png"} alt={""} layout='fill'/>
+                <Image className="absolute w-screen h-screen" src={"/img/IslandInMiddle.png"} alt={""} layout='fill'/>
+                {/* <Image className="absolute w-screen h-screen" src={"/img/IslandInMiddle2.png"} alt={""} layout='fill'/> */}
+
+                <Image className="absolute w-screen h-screen WaterMovingMiddle" src={"/img/Skum.png"} alt={""} layout='fill'/>
                 <Image className="absolute w-screen h-screen" src={"/img/BgCoreOverlay.png"} alt={""} layout='fill'/>
             </div>
         </>
